3775ea98e5bb8ad4000198610df599df160c6a63
Tarun <tarunve@amdocs.com>
2020-09-16 00:16:54 +0530

    Algorithm Analysis notes

---
+++ java-basics-003-datastructure/src/main/java/complexity/T_002_TimeFunction.java
@@ -0,0 +1,15 @@
+package complexity;
+
+/**
+ *  Types of Time Functions:
+ *  =======================
+ *      O(1)            ---     Constant
+ *      O(lon(n)        ---     logrithemic
+ *      O(n)            ---     linear
+ *      O(n^2)          ---     quadratic
+ *      O(n^3)          ---     cubic
+ *      O(2^n), O(n^n)  ---     exponential
+ *
+ *      1 < logn < root(n) < n < nlogn < n^2 < n^3 .... < 2^n < 3^n <... < n^n
+ */
+public class T_002_TimeFunction {}
