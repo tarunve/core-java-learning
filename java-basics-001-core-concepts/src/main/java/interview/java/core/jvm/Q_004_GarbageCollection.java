977215d9f7279445876bb2e0eb23b32979153215
tarun <tarun.verma151@gmail.com>
2021-05-07 09:27:43 +0530

    deleting duplicate files from another repo.

--- java-basics-001-core-concepts/src/main/java/interview/java/core/jvm/Q_004_GarbageCollection.java
+++
@@ -1,7 +0,0 @@
-package interview.java.core.jvm;
-
-/**
- *  Well Explained here :
- *      https://www.oracle.com/webfolder/technetwork/tutorials/obe/java/gc01/index.html
- */
-public class Q_004_GarbageCollection {}
