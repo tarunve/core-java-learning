65e18d6282407a4cd0f139ef698b519127754980
“tarunve” <tarun.verma151@gmail.com>
2021-12-02 23:55:16 +0530

    notes observable

---
+++ java-basics-005-enhancements-10/src/main/java/com/learning/tutorials/rxjava/T001_ReactiveProgrammingBasics.java
@@ -0,0 +1,29 @@
+package com.learning.tutorials.rxjava;
+
+/**
+ *  Attributes of Reactive Applications:
+ *  -----------------------------------
+ *  1.  Event Driven :
+ *      -   Based on Observer Pattern.
+ *      -   Observable acts as Subject and Observer subscribe to observable.
+ *  2.  Scalable
+ *      -   Improved performance with concurrency using Observables which will execute concurrently.
+ *  3.  Resilient
+ *      -   Graceful error handling.
+ *      -   Manage failure.
+ *      -   RxJava onError handlers.
+ *  4.  Responsive
+ *      -   slow response == failure
+ *      -   Metrics to actively monitor performance.
+ *      -   Define desired levels of performance.
+ *
+ *  Observable Lifecycle:
+ *  --------------------
+ *  -   Assembly: Setup observable
+ *  -   Subscription: onSubscribe, Capture Disposable
+ *  -   Generation: Active emission of events
+ *
+ *  -   RxJava is single threaded by default. Even when you specify a scheduler, in most cases RxJava
+ *      will still only work on a single thread of the designated scheduler.
+ */
+public class T001_ReactiveProgrammingBasics { }
---
+++ java-basics-005-enhancements-10/src/main/java/com/learning/tutorials/rxjava/T002_ObservableCompositionAndFiltering.java
@@ -0,0 +1,22 @@
+package com.learning.tutorials.rxjava;
+
+/**
+ *  Hot vs Cold Observables:
+ *  -----------------------
+ *  -   Cold observable are those which emits an event when someone subscribe to it.
+ *      Like listening to podcast. Each subscriber can hear the whole podcast.
+ *  -   Hot observable are those which emits an event when no one is listening.
+ *      Like watching broadcast TV without a DVR. You only get the data from the
+ *      point you tune in.
+ *
+ *  Observables Creation Methods:
+ *  -----------------------------
+ *  -   just : from single values.
+ *          Observable.just(42)
+ *  -   fromXXXX
+ *          Observable.fromArray(Long[])
+ *          Observable.fromIterable(ArrayList)
+ *  -   create
+ *  -   generate
+ */
+public class T002_ObservableCompositionAndFiltering { }
