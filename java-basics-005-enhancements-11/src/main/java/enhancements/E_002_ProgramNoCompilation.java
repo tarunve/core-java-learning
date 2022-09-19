e03fb30dd9889c232a2dbf3edbc42316fadcd833
tarunve <tarun.verma151@gmail.com>
2022-09-19 19:44:36 +0530

    java 11 enhancement

---
+++ java-basics-005-enhancements-11/src/main/java/enhancements/E_002_ProgramNoCompilation.java
@@ -0,0 +1,12 @@
+package enhancements;
+
+/**
+ *  -   we can execute Java source code contained in a single file without the need to compile
+ *      it first.
+ */
+public class E_002_ProgramNoCompilation {
+
+    public static void main(String[] args) {
+        System.out.println("This is testing class.");
+    }
+}
