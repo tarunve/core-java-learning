17f00aa963f6898ad96d1257ef8bfcb44c115638
tarunve <tarun.verma151@gmail.com>
2024-03-19 23:02:28 +0530

    refactoring code

--- java-basics-002-design-patterns/src/main/java/com/java/basics/designpatterns/solid/T_003_OpenClosedPrinciple.java
+++ java-basics-002-design-patterns/src/main/java/com/java/basics/designpatterns/solid/T_003_OpenClosedPrinciple.java
@@ -3,18 +3,18 @@ package com.java.basics.designpatterns.solid;
 import lombok.Data;
 
 /**
- * 	->	The Open/Closed Principle is the “O” of SOLID’s five software design principles. It was Bertrand Meyer 
- * 		who coined the term in his book “Object-Oriented Software Construction”. 
+ * 	->	The Open/Closed Principle is the â€œOâ€ of SOLIDâ€™s five software design principles. It was Bertrand Meyer 
+ * 		who coined the term in his book â€œObject-Oriented Software Constructionâ€. 
  * 	->	The Open/Closed Principle states that classes, modules, microservices, and other code units should be 
  * 		open for extension but closed for modification.
  * 	->	So, you should be able to extend your existing code using OOP features like inheritance via subclasses 
  * 		and interfaces. However, you should never modify classes, interfaces, and other code units that already 
  * 		exist (especially if you use them in production), as it can lead to unexpected behavior. If you add a 
  * 		new feature by extending your code rather than modifying it, you minimize the risk of failure as much 
- * 		as possible. Besides, you also don’t have to unit test existing functionalities.
+ * 		as possible. Besides, you also donâ€™t have to unit test existing functionalities.
  * 	->	For example, spring framework has class DispatcherServlet. This class acts as front controller for 
  * 					 String based web applications. To use this class, we are not required to modify this class. 
- * 		All we need is to pass initialization parameters and we can extend it’s functionality the way we want.
+ * 		All we need is to pass initialization parameters and we can extend itâ€™s functionality the way we want.
  */
 public class T_003_OpenClosedPrinciple {
 
